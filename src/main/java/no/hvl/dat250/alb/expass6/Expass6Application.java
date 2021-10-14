01d3c8cc5d43993e69b02dc82e0251aa0a9ad3a1
framo-alb <47555931+framo-alb@users.noreply.github.com>
2021-10-14 04:22:07 +0000

    first commit

---
+++ src/main/java/no/hvl/dat250/alb/expass6/Expass6Application.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.alb.expass6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Expass6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Expass6Application.class, args);
+	}
+
+}
---
+++ src/test/java/no/hvl/dat250/alb/expass6/Expass6ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.alb.expass6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expass6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
